86fec3a2026811d4544551b8f866ace871cd47a6
Adrian Heine <mail@adrianheine.de>
2023-12-04 14:11:02 +0100

    [Day1] Clippy

--- src/bin/day1_part1.rs
+++ src/bin/day1_part1.rs
@@ -4,14 +4,14 @@ fn calc(input: impl Iterator<Item = u8>) -> usize {
     let mut sum: usize = 0;
     let mut last: Option<u8> = None;
     for c in input {
-        if c >= b'0' && c <= b'9' {
+        if c.is_ascii_digit() {
             let v = c - b'0';
             if last.is_none() {
-                sum = sum + (10 * v as usize);
+                sum += 10 * v as usize;
             }
             last = Some(v);
         } else if c == b'\n' || c == b'\r' {
-            sum = sum + last.unwrap() as usize;
+            sum += last.unwrap() as usize;
             last = None;
         }
     }
@@ -19,5 +19,5 @@ fn calc(input: impl Iterator<Item = u8>) -> usize {
 }
 
 fn main() {
-    println!("{}", calc(io::stdin().lock().bytes().map(|v| v.unwrap())))
+    println!("{}", calc(io::stdin().lock().bytes().map(Result::unwrap)));
 }
