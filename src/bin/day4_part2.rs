b49a2ccaa7597db38d79cc09156687c6ab78cc67
Adrian Heine <mail@adrianheine.de>
2023-12-05 11:23:58 +0100

    [Day4] Part 2

---
+++ src/bin/day4_part2.rs
@@ -0,0 +1,89 @@
+use std::collections::VecDeque;
+use std::io;
+
+fn eat(line: &mut &[u8], c: u8) {
+    let mut idx = 0;
+    while line[idx] == c {
+        idx += 1;
+    }
+    *line = &line[idx..];
+}
+
+fn get_number(line: &mut &[u8]) -> usize {
+    let mut n = 0;
+    let mut idx = 0;
+    while idx < line.len() && line[idx].is_ascii_digit() {
+        n = n * 10 + (line[idx] - b'0') as usize;
+        idx += 1;
+    }
+    *line = &line[idx..];
+    n
+}
+
+fn line_points(mut line: &[u8]) -> usize {
+    let mut my_numbers = Vec::with_capacity(10);
+    let mut points = 0;
+    line = &line[line
+        .iter()
+        .enumerate()
+        .find(|(_, v)| **v == b':')
+        .unwrap()
+        .0
+        + 1..];
+    eat(&mut line, b' ');
+    while line[0] != b'|' {
+        my_numbers.push(get_number(&mut line));
+        eat(&mut line, b' ');
+    }
+    line = &line[1..]; // Eat '|'
+    while !line.is_empty() {
+        eat(&mut line, b' ');
+        let n = get_number(&mut line);
+        if let Some((idx, _)) = my_numbers.iter().enumerate().find(|(_, v)| **v == n) {
+            my_numbers.swap_remove(idx);
+            points += 1;
+        }
+    }
+    points
+}
+
+fn calc(input: impl Iterator<Item = impl AsRef<str>>) -> usize {
+    let mut sum: usize = 0;
+    let mut lines: VecDeque<usize> = VecDeque::new();
+    for line in input {
+        let instance_count = lines.pop_front().unwrap_or_default() + 1;
+        let line = line.as_ref().as_bytes();
+        let points = line_points(line);
+        for i in 0..points {
+            if let Some(i) = lines.get_mut(i) {
+                *i += instance_count;
+            } else {
+                lines.push_back(instance_count);
+            }
+        }
+        sum += instance_count;
+    }
+    assert!(lines.is_empty());
+    sum
+}
+
+fn main() {
+    println!("{}", calc(io::stdin().lines().map(Result::unwrap)));
+}
+
+#[test]
+fn test() {
+    assert_eq!(
+        calc(
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"
+            .lines()
+        ),
+        30
+    );
+}
